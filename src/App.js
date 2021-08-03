4bc5b1f733e949a1313be9be1f7fbfc649968321
DevSIAC <ray_tony@iMac-de-DevSIAC.local>
2021-08-03 16:34:49 +0100

    Adcionando Rotas com componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
-import {BrowserRouter as Router,Link} from 'react-router-dom';
+import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
+import {Primeiro} from './components/Primeiro/'
 function App() {
   return (
     
     <Router>
       <Link to='/primeiro'>Primeiro LInk</Link>
       <Link to='/segundo'>Segundo LInk</Link>
-      <Link to='/Terceiro'>Primeiro LInk</Link>
+      <Link to='/terceiro'>Primeiro LInk</Link>
       <h1>Olá Mundo</h1>
+      <hr/>
+      <Switch>
+        <Route path='/primeiro'>
+         <Primeiro/>
+        </Route>
+        <Route path='/segundo'>
+         <h2>Carregou o segundo Link</h2>
+        </Route>
+        <Route path='/terceiro'>
+         <h2>Carregou o terceiro Link</h2>
+        </Route>
+      </Switch>
     </Router>
+    
+
     
     
   );
---
+++ src/components/Primeiro/index.js
@@ -0,0 +1,6 @@
+import {React} from 'react';
+ export const Primeiro = () =>(
+  <h2>Carregou o primeiro Link, Usando Componente</h2>
+ );
+
+ export default Primeiro;
